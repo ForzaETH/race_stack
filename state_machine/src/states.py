9d7f0294a844be09655b9e1c59211a5982136957
nibauman <nicolas.baumann@pbl.ee.ethz.ch>
2024-10-02 14:09:14 +0000

    minor state machine updates

--- state_machine/src/states.py
+++ state_machine/src/states.py
@@ -1,15 +1,15 @@
 from state_machine_node import StateMachine
-from f110_msgs.msg import WpntArray
+from f110_msgs.msg import WpntArray, Wpnt
 
 """
 Here we define the behaviour in the different states.
 Every function should be fairly concise, and output an array of f110_msgs.Wpnt
 """
-def GlobalTracking(state_machine: StateMachine):
+def GlobalTracking(state_machine: StateMachine) -> List[Wpnt]:
     s = int(state_machine.cur_s/state_machine.waypoints_dist + 0.5)
     return [state_machine.glb_wpnts[(s + i)%state_machine.num_glb_wpnts] for i in range(state_machine.n_loc_wpnts)]
 
-def Trailing(state_machine: StateMachine):
+def Trailing(state_machine: StateMachine) -> List[Wpnt]:
     # This allows us to trail on the last valid spline if necessary
     if (state_machine.ot_planner == "spliner" or state_machine.ot_planner == "predictive_spliner") and state_machine.last_valid_avoidance_wpnts is not None:
         splini_wpts = state_machine.get_splini_wpts()
@@ -19,7 +19,7 @@ def Trailing(state_machine: StateMachine):
         s = int(state_machine.cur_s/state_machine.waypoints_dist + 0.5)
         return [state_machine.glb_wpnts[(s + i)%state_machine.num_glb_wpnts] for i in range(state_machine.n_loc_wpnts)]
 
-def Overtaking(state_machine: StateMachine):
+def Overtaking(state_machine: StateMachine) -> List[Wpnt]:
     if (state_machine.ot_planner == "spliner" or state_machine.ot_planner == "predictive_spliner"):
         splini_wpts = state_machine.get_splini_wpts()
         s = int(state_machine.cur_s/state_machine.waypoints_dist + 0.5)
@@ -35,4 +35,5 @@ def Overtaking(state_machine: StateMachine):
         return [state_machine.overtake_wpnts[(s + i)%state_machine.num_ot_points] for i in range(state_machine.n_loc_wpnts)]
 
 def FTGOnly(state_machine: StateMachine):
+    """No waypoints are generated in this follow the gap only state, all the control inputs are generated in the control node."""
     return []
