310c4867b2033a7c304965ede73e6672e9e92379
Edoardo Ghignone <eghignone@pbl.ee.ethz.ch>
2025-08-25 09:30:41 +0000

    Fix acados/tera_renderer version to 0.0.34
    An older version of tera_renderer is needed because glibc>=2.32 is natively occuring in Ubuntu distributions later than the one we need for Noetic.
    Also some minor fixes and qol improvements to the acados installation are included.

--- .devcontainer/.install_utils/install_acados.sh
+++ .devcontainer/.install_utils/install_acados.sh
@@ -4,13 +4,13 @@
 ACADOS_ROOT=${1:-/acados}
 
 # Clone acados into the target folder
-git clone https://github.com/acados/acados.git $ACADOS_ROOT
-cd $ACADOS_ROOT
+git clone --branch v0.4.4 https://github.com/acados/acados.git "$ACADOS_ROOT"
+cd "$ACADOS_ROOT" || exit
 git submodule update --init --recursive
 
 # Build and install acados
 mkdir -p build
-cd build
+cd build || exit
 cmake -DACADOS_WITH_QPOASES=ON .. # Add more options as needed
 make install -j4
 
@@ -18,8 +18,15 @@ make install -j4
 make shared_library
 
 # Install Python interface
-pip install $ACADOS_ROOT/interfaces/acados_template
+pip install "$ACADOS_ROOT"/interfaces/acados_template
 
 # Add environment variables to .bashrc and/or .zshrc
 echo "export ACADOS_SOURCE_DIR=$ACADOS_ROOT" >> ~/.bashrc
 echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$ACADOS_ROOT/lib" >> ~/.bashrc
+
+# Manually install older version of tera_renderer because ubuntu 20.04 (needed for Noetic) doesn't have glibc >= 2.32
+wget https://github.com/acados/tera_renderer/releases/download/v0.0.34/t_renderer-v0.0.34-linux -O $ACADOS_ROOT/bin/t_renderer
+chmod +x $ACADOS_ROOT/bin/t_renderer
+
+echo "Acados installation completed."
+echo "Run 'source ~/.bashrc' or 'source ~/.zshrc' depending on your shell."
