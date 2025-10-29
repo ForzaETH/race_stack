0b140cb51da65be0b6c75c477bd7ad95b8bc5786
Edoardo Ghignone <eghignone@pbl.ee.ethz.ch>
2025-10-29 17:22:26 +0000

    update publisher to reduce publishing

--- planner/global_planner/global_planner/global_trajectory_publisher.py
+++ planner/global_planner/global_planner/global_trajectory_publisher.py
@@ -19,44 +19,50 @@ class GlobalRepublisher(Node):
         self.glb_markers = None
         self.glb_wpnts = None
         self.track_bounds = None
+        self.publish_markers = self.get_parameter('publish_markers').get_parameter_value().bool_value
+        
 
-        self.create_subscription(WpntArray, '/global_waypoints', self.glb_wpnts_cb, 10)
-        self.create_subscription(MarkerArray, '/global_waypoints/markers', self.glb_markers_cb, 10)
-        self.create_subscription(MarkerArray, '/trackbounds/markers', self.bounds_cb, 10)
+        #Declare local ros params
+        self.declare_parameter(name='track_length', value=6.9)
 
-        self.glb_wpnts_pub = self.create_publisher(WpntArray, 'global_waypoints', 10)
-        self.glb_markers_pub = self.create_publisher(MarkerArray, 'global_waypoints/markers', 10)
-        self.vis_track_bnds = self.create_publisher(MarkerArray, 'trackbounds/markers', 10)
+        self.glb_wpnts_pub = self.create_publisher(WpntArray, '/global_waypoints', 10)
+        if self.publish_markers:
+            self.glb_markers_pub = self.create_publisher(MarkerArray, '/global_waypoints/markers', 10)
+            self.vis_track_bnds = self.create_publisher(MarkerArray, '/trackbounds/markers', 10)
 
         # shortest_path
-        self.glb_sp_markers = None
-        self.glb_sp_wpnts = None
-        self.create_subscription(WpntArray, '/global_waypoints/shortest_path', self.glb_sp_wpnts_cb, 10)
-        self.create_subscription(MarkerArray, '/global_waypoints/shortest_path/markers', self.glb_sp_markers_cb, 10)
-        self.glb_sp_wpnts_pub = self.create_publisher(WpntArray, 'global_waypoints/shortest_path', 10)
-        self.glb_sp_markers_pub = self.create_publisher(MarkerArray, 'global_waypoints/shortest_path/markers', 10)
+        self.publish_shortest_path = self.get_parameter('publish_shortest_path').get_parameter_value().bool_value
+        if self.publish_shortest_path:
+            self.get_logger().info("Global Republisher: Publishing shortest path waypoints and markers")
+            self.glb_sp_markers = None
+            self.glb_sp_wpnts = None
+            self.glb_sp_wpnts_pub = self.create_publisher(WpntArray, '/global_waypoints/shortest_path', 10)
+            if self.publish_markers:
+                self.glb_sp_markers_pub = self.create_publisher(MarkerArray, '/global_waypoints/shortest_path/markers', 10)
 
         # centerline
-        self.centerline_wpnts = None
-        self.centerline_markers = None
-        self.create_subscription(WpntArray, '/centerline_waypoints', self.centerline_wpnt_cb, 10)
-        self.create_subscription(MarkerArray, '/centerline_waypoints/markers', self.centerline_markers_cb, 10)
-        self.centerline_wpnts_pub = self.create_publisher(WpntArray, '/centerline_waypoints', 10)
-        self.centerline_markers_pub = self.create_publisher(MarkerArray, '/centerline_waypoints/markers', 10)
+        self.publish_centerline = self.get_parameter('publish_centerline').get_parameter_value().bool_value
+        if self.publish_centerline:
+            self.get_logger().info("Global Republisher: Publishing centerline waypoints and markers")
+            self.centerline_wpnts = None
+            self.centerline_markers = None
+            self.centerline_wpnts_pub = self.create_publisher(WpntArray, '/centerline_waypoints', 10)
+            if self.publish_markers:
+                self.centerline_markers_pub = self.create_publisher(MarkerArray, '/centerline_waypoints/markers', 10)
 
         # map infos
         self.map_infos = None
-        self.create_subscription(String, '/map_infos', self.map_info_cb, 10)
         self.map_info_pub = self.create_publisher(String, 'map_infos', 10)
 
         self.est_lap_time = None
-        self.create_subscription(Float32, 'estimated_lap_time', self.est_lap_time_cb, 10)
         self.est_lap_time_pub = self.create_publisher(Float32, 'estimated_lap_time', 10)
 
-        # graph lattice 
-        self.graph_lattice = None
-        self.create_subscription(MarkerArray, '/lattice_viz', self.lattice_cb, 10)
-        self.lattice_pub = self.create_publisher(MarkerArray, '/lattice_viz', 10)
+        # graph lattice
+        self.publish_lattice = self.get_parameter('publish_lattice').get_parameter_value().bool_value
+        if self.publish_lattice:
+            self.get_logger().info("Global Republisher: Publishing graph lattice")
+            self.graph_lattice = None
+            self.lattice_pub = self.create_publisher(MarkerArray, '/lattice_viz', 10)
 
         # Read info from json file if it is provided, so everything is always published
         map_path = self.get_parameter('map_path').get_parameter_value().string_value
@@ -72,10 +78,10 @@ class GlobalRepublisher(Node):
                 self.get_logger().warn(f"{map_path} param not found. Not publishing")
         else:
             self.get_logger().info(f"global_trajectory_publisher did not find any map_path param {map_path}")
-
-        # Publish at 2 Hz
-        self.create_timer(0.5, self.global_republisher)
-
+        
+        # Publish at 0.1 Hz
+        self.create_timer(10, self.global_republisher)
+        
     def glb_wpnts_cb(self, msg):
         self.glb_wpnts = msg
         track_length = msg.wpnts[-1].s_m
@@ -111,24 +117,41 @@ class GlobalRepublisher(Node):
 
     def global_republisher(self):
 
-        if self.glb_wpnts is not None and self.glb_markers is not None:
+        # Publish global waypoints
+        if self.glb_wpnts is not None:
             self.glb_wpnts_pub.publish(self.glb_wpnts)
+        if self.publish_markers and self.glb_markers is not None and hasattr(self, 'glb_markers_pub'):
             self.glb_markers_pub.publish(self.glb_markers)
-        if self.glb_sp_wpnts is not None and self.glb_sp_markers is not None:
-            self.glb_sp_wpnts_pub.publish(self.glb_sp_wpnts)
-            self.glb_sp_markers_pub.publish(self.glb_sp_markers)
-        if self.centerline_wpnts is not None and self.centerline_markers is not None:
-            self.centerline_wpnts_pub.publish(self.centerline_wpnts)
-            self.centerline_markers_pub.publish(self.centerline_markers)
-        if self.track_bounds is not None:
+        
+        # Publish shortest path
+        if self.publish_shortest_path:
+            if self.glb_sp_wpnts is not None and hasattr(self, 'glb_sp_wpnts_pub'):
+                self.glb_sp_wpnts_pub.publish(self.glb_sp_wpnts)
+            if self.publish_markers and self.glb_sp_markers is not None and hasattr(self, 'glb_sp_markers_pub'):
+                self.glb_sp_markers_pub.publish(self.glb_sp_markers)
+        
+        # Publish centerline
+        if self.publish_centerline:
+            if self.centerline_wpnts is not None and hasattr(self, 'centerline_wpnts_pub'):
+                self.centerline_wpnts_pub.publish(self.centerline_wpnts)
+            if self.publish_markers and self.centerline_markers is not None and hasattr(self, 'centerline_markers_pub'):
+                self.centerline_markers_pub.publish(self.centerline_markers)
+        
+        # Publish track bounds
+        if self.publish_markers and self.track_bounds is not None and hasattr(self, 'vis_track_bnds'):
             self.vis_track_bnds.publish(self.track_bounds)
+        
+        # Publish map info and lap time (always)
         if self.map_infos is not None:
             self.map_info_pub.publish(self.map_infos)
         if self.est_lap_time is not None:
             self.est_lap_time_pub.publish(self.est_lap_time)
-        if self.graph_lattice is not None:
+        
+        # Publish lattice
+        if self.publish_lattice and self.graph_lattice is not None and hasattr(self, 'lattice_pub'):
             self.lattice_pub.publish(self.graph_lattice)
 
+         
 
 def main(args=None):
     rclpy.init(args=args)
--- planner/global_planner/setup.py
+++ planner/global_planner/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'global_planner'
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/**/*.xml', recursive=True)),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
