e1336e24c8e52d963f1a22a2ba4d508884658993
Saimaneesh <yeturu.saimaneesh@gmail.com>
2026-09-26 10:04:37 +0000

    recursively clone in external repos

--- .install_utils/post_create_command.sh
+++ .install_utils/post_create_command.sh
@@ -6,8 +6,21 @@ set -e
 # Setup user permissions
 sudo chown -R race_crew:race_crew /ws/
 
-# Clone external repositories into /ws/src (no submodules)
+# Clone repositories into src
+sudo apt-get update
 vcs import /ws/src < /ws/src/race_stack/.install_utils/dependencies.repos
+
+# Initialize submodules in all imported Git repositories. 
+# Note that we avoid this on the race_stack, but for external repositories we do not handle it
+while IFS= read -r -d '' git_dir; do
+        git -C "$(dirname "$git_dir")" submodule update --init --recursive
+done < <(find /ws/src -type d -name .git -print0)
+
+# Import manifests shipped by cloned repositories. Their paths are relative to /ws.
+while IFS= read -r repos_file; do
+	vcs import /ws < "$repos_file"
+done < <(find /ws/src -mindepth 2 -name '*.repos' \
+	-not -path '/ws/src/race_stack/.install_utils/dependencies.repos' -print)
 
 # Install ROS 2 dependencies (race_stack and additional cloned repos)
 rosdep update
